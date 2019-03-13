bbbd639f1d8bca0e12ea6a3711278687bac9f33f
Alexander Olofsson <alexander.olofsson@liu.se>
2019-03-13 09:52:35 +0100

    Use actual service handling for workers

--- manifests/service.pp
+++ manifests/service.pp
@@ -56,22 +56,25 @@ class misp::service inherits misp {
       bind           => $misp::redis_host,
       requirepass    => $misp::redis_password,
       port           => $misp::redis_port,
-      notify         => Exec['start bg workers', 'restart bg workers'],
+      notify         => Service['misp-workers'],
     }
   }
 
-  exec {'start bg workers':
-    command => "/usr/bin/su -s /bin/bash ${misp::default_user} -c '/usr/bin/scl enable rh-${misp::php_version} ${misp::install_dir}/app/Console/worker/start.sh'",
-    unless  => "/usr/bin/su -s /bin/bash ${misp::default_user} -c '/usr/bin/scl enable rh-${misp::php_version} ${misp::install_dir}/app/Console/worker/status.sh'",
-    user    => $misp::default_high_user,
-    group   => $misp::default_high_group,
-  }
+  file { '/etc/systemd/system/misp-workers.service':
+    ensure  => file,
+    content => epp('misp/misp-workers.service.epp', {
 
-  exec {'restart bg workers':
-    command     => "/usr/bin/su -s /bin/bash ${misp::default_user} -c '/usr/bin/scl enable rh-${misp::php_version} ${misp::install_dir}/app/Console/worker/start.sh'",
-    user        => $misp::default_high_user,
-    group       => $misp::default_high_group,
-    refreshonly => true,
-    subscribe   => Exec['CakeResque install'],
+        user        => $misp::default_user,
+        group       => $misp::default_group,
+        php_version => $misp::php_version,
+        services    => [$misp::mariadb_service, 'redis'],
+        scls        => [$misp::mariadb_scl],
+    }),
+    notify  => Service['misp-workers'],
+  }
+  service { 'misp-workers':
+    ensure    => running,
+    enable    => true,
+    subscribe => Exec['CakeResque install'],
   }
 }
